2962281d1ec7a0c56d288eea2d98c4a425ee5985
Anudeep varma <datlanagaanudeep@gmail.com>
2025-07-18 22:57:57 +0530

    modified component to get github url

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
 module "user" {
  for_each = var.components
- source = "../../terraform-aws-roboshop"
+ source = "git::https://github.com/Varma224/terraform-aws-roboshop.git?ref=main"
  component = each.key
  rule_priority = each.value.rule_priority
 
