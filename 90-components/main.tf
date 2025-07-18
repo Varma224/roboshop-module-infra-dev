f16a473d346b50adcfc702f72a382ae2ebaa7f28
Anudeep varma <datlanagaanudeep@gmail.com>
2025-07-18 23:50:48 +0530

    modified file

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
 module "component" {
- for_each = var.components
  source = "git::https://github.com/Varma224/terraform-aws-roboshop.git?ref=main"
+ for_each = var.components
  component = each.key
  rule_priority = each.value.rule_priority
 
